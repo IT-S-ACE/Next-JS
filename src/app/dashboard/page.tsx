4582aa31e0fdb76f521bb50af615af08bc6955a7
ACE <hanan.a.masoud2003@gmail.com>
2024-10-17 18:05:37 +0300

    - File Colocation

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+import LineChart from "./line-chart"
+
+function BarChart() {
+    return (
+        <h1>
+            BarChart component
+        </h1>
+    )
+}
+
+export default function Dashboard() {
+    return (
+        <>
+            <h1>
+                Dashoard
+            </h1>
+            <LineChart />
+        </>
+    )
+};
