53c04be4761840fc4c5c759a0cd67483c88c2241
ACE <hanan.a.masoud2003@gmail.com>
2024-10-19 14:48:49 +0300

    - Routnig Metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title: 'About ACE'
+}
+
 export default function About () {
     return <h1>About Page</h1>
 }
--- src/app/product/[productID]/page.tsx
+++ src/app/product/[productID]/page.tsx
@@ -1,7 +1,20 @@
+import { Metadata } from "next"
+
 type TParams = {
-    params : { productID : string }
+    params: { productID: string }
 }
 
-export default function ProductDetails ( {params} : TParams ) {
+export const generateMetadata = async ({ params }: TParams): Promise<Metadata> => {
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productID}`)
+        }, 100);
+    })
+    return {
+        title: `Product ${title}`,
+    }
+}
+
+export default function ProductDetails({ params }: TParams) {
     return <h1>Details about Product {params.productID}</h1>
 }
