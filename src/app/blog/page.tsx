5de04b6cc7a01611a60ef90e7fd7afdbf0e15309
ACE <hanan.a.masoud2003@gmail.com>
2024-10-21 13:04:52 +0300

    - Title Metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next"
+
+export const metadata : Metadata = {
+    // title: 'Blog' // will be replaced with (%s) and showed (Blog | Codevolution)
+    title: {
+        absolute : 'Blog' // will  display in the title only Blog without the rest of the title from the layout (| Codevolution)
+    }
+}
+
 export default function Blog () {
     return <h1>Blog Page</h1>
 }
