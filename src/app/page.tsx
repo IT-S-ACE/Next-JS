d560e8db896376fd3881e0b6137135cdc252e1d8
ACE <hanan.a.masoud2003@gmail.com>
2024-10-22 12:15:58 +0300

    - Link Component Navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link"
+
 export default function Home () {
-    return <h1>Home Page</h1>
+    return <>
+    <h1>Home Page</h1>
+    <Link href='/blog'>Blog</Link>
+    <br />
+    <Link href='/product'>Products</Link>
+</>
 }
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,3 +1,22 @@
-export default function Product () {
-    return <h1>Product Page</h1>
+import Link from "next/link"
+export default function Product() {
+    const productID = 100;
+    return (
+        <>
+            <Link href='/'> Home</Link>
+            <h1> Product Page</h1>
+            <h1>
+                <Link href='product/1'>Product 1</Link>
+            </h1>
+            <h1>
+                <Link href='product/2'>Product 2</Link>
+            </h1>
+            <h1>
+                <Link href='product/3'>Product 3</Link>
+            </h1>
+            <h1>
+                <Link href={`product/${productID}`}>Product {productID}</Link>
+            </h1>
+        </>
+    )
 }
