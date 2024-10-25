a4edc052ec619faf0f6b4558c13afb31d6a6b622
ACE <hanan.a.masoud2003@gmail.com>
2024-10-25 15:31:04 +0300

    - Templates the (layout.tsx) file changed to (templates.tsx)

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import './style.css'
+import { useState } from "react";
 
 const navLinks = [
     {name : 'Register', href : '/register'},
@@ -17,10 +18,12 @@ type Tchildren ={
 export default function AuthLayout({
     children
 } : Tchildren) {
+
+    const [input , setInput] = useState()
     const pathname = usePathname()
-    console.log(children)
     return (
         <div>
+            <input type="text" value={input} onChange={e => e.target.value} className="bg-slate-500 border-black border-[3px]"/>
             {navLinks.map((link) => {
                 
                 const isActive = pathname.startsWith(link.href) 
