d6673296140ae65aab01d71e57b00923deb6095d
ACE <hanan.a.masoud2003@gmail.com>
2024-10-29 12:45:18 +0300

    - Conditional Routes

---
+++ src/app/complex-dashboard/@login/page.tsx
@@ -0,0 +1,5 @@
+import Card from "@/components/card";
+
+export default function login() {
+    return <Card>Please Login first</Card>
+};
--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -1,26 +1,33 @@
+import login from "./@login/page";
+
 type TLayout = {
     children : React.ReactNode,
     user : React.ReactNode,
     revenue : React.ReactNode,
     notification : React.ReactNode,
+    login : React.ReactNode,
 }
 
 export default function ComplexDashboardLayout({
     children,
     user,
     revenue,
-    notification
+    notification,
+    login
 }:TLayout) {
-    return(
+    const isLoggedIn= true; // if isLoggedIn= false then it will show the login page
+    return isLoggedIn ?(
         <>
         <div>{children}</div>
         <div style={{display: "flex"}}>
-            <div style={{display:"flex", flexDirection: "column"}}>   
+            <div style={{display:"flex", flexDirection: "column"}}>
                 <div>{user}</div>
                 <div>{revenue}</div>
             </div>
             <div style={{display: "flex" , flex: 1}}>{notification}</div>
         </div>
         </>
+    ): (
+        login
     )
 };
